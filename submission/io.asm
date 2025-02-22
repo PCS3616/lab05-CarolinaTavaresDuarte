a69fa856c09209eda6777fe235592894d394f4ca
CarolinaTavaresDuarte <caroltd8@gmail.com>
2025-02-22 02:03:15 +0000

    estou sem tempoooo

--- submission/io.asm
+++ submission/io.asm
@@ -6,36 +6,30 @@ HM /0000 ; Halt (fim da execução)
 K /0000 ; Endereço de referência
 
 GD /110 ; Lê os dois primeiros dígitos (ex: "07")
+SB /108 ; subtrai 0x3030 para converter ASCII para decimal
 MM /100 ; Armazena X em ASCII
 
 GD /110 ; Lê os espaços entre os números
 GD /110 ; Lê os últimos dois dígitos (ex: "54")
+SB /108 ; subtrai 0x3030 para converter ASCII para decimal
 MM /102 ; Armazena Y em ASCII
 
 LD /100 ; Carrega X
-SB /108 ; Subtrai 0x3030 para converter de ASCII para decimal
-MM /100 ; Armazena X convertido
+AD /102 ; Soma Y convertido
+MM /104 ; Armazena a soma
 
-LD /102 ; Carrega Y
-SB /108 ; Subtrai 0x3030 para converter de ASCII para decimal
-MM /102 ; Armazena Y convertido
+LD /104 ; Carrega soma
+SB /10A ; Verifica d'igito menos significativo >=A
+MM /050 ; Armazena Y convertido
 
-LD /100 ; Carrega X convertido
-AD /102 ; Soma com Y convertido
-MM /104 ; Armazena o resultado da soma
-
-LD /104 ; Carrega o resultado
-SB /10A ; Verifica se o dígito menos significativo >= A
-JZ /050 ; Se não precisar de ajuste, pula
-
-SB /10A ; Subtrai 0x000A (corrige o "vai-um")
-AD /10C ; Soma 0x0100 para o "vai-um"
-MM /104 ; Atualiza o resultado
+SB /10A 
+AD /10C
+MM /104 ; Atualiza o resultado da soma
 
 @ /0050
 LD /104 ; Carrega o resultado da soma
 AD /108 ; Soma 0x3030 para converter de volta para ASCII
-PD /112 ; Imprime o resultado no monitor
+PD /110 ; Imprime o resultado no monitor
 HM /0000 ; Halt (fim da execução)
 
 @ /0100
