95664e2ced7ebb077e702cc8986721a24ff085f8
CarolinaTavaresDuarte <141371244+CarolinaTavaresDuarte@users.noreply.github.com>
2025-02-22 00:43:58 -0300

    Update io.asm

--- submission/io.asm
+++ submission/io.asm
@@ -6,14 +6,14 @@ HM /0000      ; Halt (fim da execução)
 K /0000       ; Endereço de referência
 
 GD /000       ; Lê os dois primeiros dígitos (X)
-SB /108       ; Subtrai 0x3030 para converter de ASCII para decimal
+SB /106       ; Subtrai 0x3030 para converter de ASCII para decimal
 MM /100       ; Armazena X convertido
 
 GD /000       ; Lê os espaços entre os números (descarta)
 MM /10E       ; armazena os espaços
 GD /000       ; Lê os últimos dois dígitos (Y)
 MM /102       ; armazena Y
-SB /108       ; Subtrai 0x3030 para converter de ASCII para decimal
+SB /106       ; Subtrai 0x3030 para converter de ASCII para decimal
 MM /102       ; Armazena Y convertido
 
 LD /100       ; Carrega X convertido
@@ -28,24 +28,24 @@ JN /050      ; Se não precisar de ajuste, pula
 AD /10A       ; Soma 0x0100 para o "vai-um"
 MM /104       ; Atualiza o resultado
 LD /104       ; carreguei o resultado
-AD /108       ; adicionar para o algoritmo
+AD /106       ; adicionar para o algoritmo
 PD /100       ; printei o final
 RS /00E
 
 @ /0050
 LD /104       ; Carrega o resultado final
-AD /108       ; Soma 0x3030 para converter de volta para ASCII
+AD /106       ; Soma 0x3030 para converter de volta para ASCII
 PD /100       ; Imprime o resultado no monitor
 RS /00E       ; Retorna da sub-rotina
 
 @ /0100
-K /0000       ; Primeiro número (X)
-K /0000       ; Segundo número (Y)
-K /0000       ; Resultado da soma
+K /0000       ; Primeiro número (X) (100)
+K /0000       ; Segundo número (Y) (102)
+K /0000       ; Resultado da soma (104)
 
 @ /0106
-K /3030       ; Constante ASCII (-0x3030)
-K /000A       ; Constante para ajuste do "vai-um"
-K /0100       ; Constante para somar 0x0100 no "vai-um"
-K /0000       ; memoria auxiliar
-k /0000       ; espaços
+K /3030       ; Constante ASCII (-0x3030) (106)
+K /000A       ; Constante para ajuste do "vai-um" (108)
+K /0100       ; Constante para somar 0x0100 no "vai-um" (10A)
+K /0000       ; memoria auxiliar (10C)
+k /0000       ; espaços (10E)
